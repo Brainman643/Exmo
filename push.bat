5060d87e3e66020136132f49d28b936a14503e0e
Evgeny Morozov <brainman@yandex.ru>
2020-06-20 20:26:28 +0300

    fix symbols pushing (#5)

--- push.bat
+++ push.bat
@@ -1,2 +1,3 @@
 echo off
 dotnet nuget push Exmo\bin\Release\*.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
+dotnet nuget push Exmo\bin\Release\*.snupkg --api-key %1 --source https://api.nuget.org/v3/index.json
