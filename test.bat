a268077e8357d0223145fc5e2b06afa0ffecfb4f
Evgeny Morozov <brainman@yandex.ru>
2020-06-23 00:29:08 +0300

    workaround for coverlet bug

--- test.bat
+++ test.bat
@@ -1,11 +1,6 @@
 cd %~dp0
 setlocal
 set GITHUB_ACTIONS=false
-echo GITHUB_ACTIONS=%GITHUB_ACTIONS%
 dotnet test -c Release --logger "trx;LogFileName=TestResults.trx" --results-directory ./TestResults /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=../TestResults/
-echo dir TestResults
-dir TestResults
 reportgenerator -reports:TestResults/coverage.info -targetdir:TestResults/Report
-echo dir TestResults
-dir TestResults
 endlocal
