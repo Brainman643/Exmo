74e9a97c7986915277645010b30132885a00757a
Evgeny Morozov <brainman@yandex.ru>
2020-06-23 00:18:03 +0300

    debug

--- test.bat
+++ test.bat
@@ -1,10 +1,11 @@
 cd %~dp0
-rmdir /s /q TestResults
+setlocal
+set GITHUB_ACTIONS=false
 echo GITHUB_ACTIONS=%GITHUB_ACTIONS%
-echo GITHUB_ACTIONS1=%GITHUB_ACTIONS1%
 dotnet test -c Release --logger "trx;LogFileName=TestResults.trx" --results-directory ./TestResults /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=../TestResults/
 echo dir TestResults
 dir TestResults
 reportgenerator -reports:TestResults/coverage.info -targetdir:TestResults/Report
 echo dir TestResults
 dir TestResults
+endlocal
