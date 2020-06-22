833a80742917688a24c099efebd5d1c8d1cfb516
Evgeny Morozov <brainman@yandex.ru>
2020-06-23 00:06:05 +0300

    debug messages

--- test.bat
+++ test.bat
@@ -1,4 +1,7 @@
 cd %~dp0
 rmdir /s /q TestResults
+echo GITHUB_ACTIONS=%GITHUB_ACTIONS%
 dotnet test -c Release --logger "trx;LogFileName=TestResults.trx" --results-directory ./TestResults /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=../TestResults/
+dir TestResults
 reportgenerator -reports:TestResults/coverage.info -targetdir:TestResults/Report
+dir TestResults
