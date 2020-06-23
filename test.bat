d846d1043137a7896330ac7e8b677e3804c49d58
Evgeny Morozov <brainman@yandex.ru>
2020-06-23 23:51:56 +0300

    Coveralls integration (#7)
    Coveralls integration

--- test.bat
+++ test.bat
@@ -1,4 +1,6 @@
 cd %~dp0
-rmdir /s /q TestResults
-dotnet test -c Release --logger "trx;LogFileName=TestResults.trx" --results-directory ./TestResults /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=../TestResults/
-reportgenerator -reports:TestResults/*.opencover.xml -targetdir:TestResults/Report
+setlocal
+set GITHUB_ACTIONS=false
+dotnet test -c Release --logger "trx;LogFileName=TestResults.trx" --results-directory ./TestResults /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=../TestResults/
+reportgenerator -reports:TestResults/coverage.info -targetdir:TestResults/Report
+endlocal
